cadc27aa398d41eb1660f046b8677a6c1f4fadec
Ygf <1548222878@qq.com>
2020-10-08 11:26:55 +0800

    第二次提交

--- lib/screens/forgot_password/components/body.dart
+++ lib/screens/forgot_password/components/body.dart
@@ -4,33 +4,51 @@
  * @Author: Yimi81
  * @Date: 2020-09-30 16:17:41
  * @LastEditors: Yimi81
- * @LastEditTime: 2020-09-30 17:44:24
+ * @LastEditTime: 2020-10-08 10:24:45
  */
 import 'package:ecommerce/common/constants.dart';
 import 'package:ecommerce/common/size_config.dart';
 import 'package:ecommerce/components/custom_suffixIcon.dart';
 import 'package:flutter/material.dart';
 
+import '../../../common/size_config.dart';
+import '../../../common/size_config.dart';
+import '../../../common/size_config.dart';
+import '../../../components/default_button.dart';
+import '../../../components/form_error.dart';
+
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        children: [
-          Text(
-            'Forgot Password',
-            style: TextStyle(
-                fontSize: getProportionateScreenWidth(28),
-                color: Colors.black,
-                fontWeight: FontWeight.bold),
+      child: SingleChildScrollView(
+        child: Padding(
+          padding:
+              EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+          child: Column(
+            children: [
+              SizedBox(
+                height: SizeConfig.screenHeight * 0.04,
+              ),
+              Text(
+                'Forgot Password',
+                style: TextStyle(
+                    fontSize: getProportionateScreenWidth(28),
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'Please enter your email an we will send \nyou a link to your account',
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: SizeConfig.screenHeight * 0.1,
+              ),
+              ForgotPassForm()
+            ],
           ),
-          Text(
-            'Please enter your email an we will send \nyou a link to your account',
-            textAlign: TextAlign.center,
-          ),
-          ForgotPassForm()
-        ],
+        ),
       ),
     );
   }
@@ -42,11 +60,13 @@ class ForgotPassForm extends StatefulWidget {
 }
 
 class _ForgotPassFormState extends State<ForgotPassForm> {
+  final _formKey = GlobalKey<FormState>();
   final List<String> errors = [];
   String email;
   @override
   Widget build(BuildContext context) {
     return Form(
+      key: _formKey,
       child: Column(
         children: [
           TextFormField(
@@ -93,6 +113,39 @@ class _ForgotPassFormState extends State<ForgotPassForm> {
                   gapPadding: 10,
                 ),
                 suffixIcon: CustomSuffixIcon(svgIcon: "assets/icons/Mail.svg")),
+          ),
+          SizedBox(
+            height: getProportionateScreenHeight(30),
+          ),
+          FormError(errors: errors),
+          SizedBox(
+            height: SizeConfig.screenHeight * 0.1,
+          ),
+          DefaultButton(
+            text: 'Continue',
+            press: () {
+              if (_formKey.currentState.validate()) {
+                print("Do what you want to do");
+              }
+            },
+          ),
+          SizedBox(
+            height: SizeConfig.screenHeight * 0.1,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Don't have an account?",
+                style: TextStyle(fontSize: getProportionateScreenWidth(16)),
+              ),
+              Text(
+                "Sign Up",
+                style: TextStyle(
+                    fontSize: getProportionateScreenWidth(16),
+                    color: kPrimaryColor),
+              )
+            ],
           )
         ],
       ),
--- lib/screens/forgot_password/forgot_password_screen.dart
+++ lib/screens/forgot_password/forgot_password_screen.dart
@@ -4,7 +4,7 @@
  * @Author: Yimi81
  * @Date: 2020-09-30 15:59:52
  * @LastEditors: Yimi81
- * @LastEditTime: 2020-09-30 16:19:22
+ * @LastEditTime: 2020-10-08 10:09:55
  */
 import 'package:ecommerce/screens/forgot_password/components/body.dart';
 import 'package:flutter/material.dart';
@@ -15,6 +15,7 @@ class ForgotPasswordScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Forgot Password'),
+        centerTitle: true,
       ),
       body: Body(),
     );
